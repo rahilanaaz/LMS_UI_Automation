e702c21e471d84c958cd44f8db0f46d807e375de
SelviGir <j.gnanaselvi@gmail.com>
2022-03-21 21:54:44 -0500

    Revert "Merge pull request #8 from rahilanaaz/Branch_Selvi"
    This reverts commit 7ade6bf202a3029dc96e4c08ed88ade36b805b36, reversing
    changes made to d18bd800bb734f6bb4acd69f45368383929971e5.

--- src/main/java/com/pages/AdminAssignmentPage.java
+++ src/main/java/com/pages/AdminAssignmentPage.java
@@ -1,43 +1,9 @@
 package com.pages;
 
-import org.openqa.selenium.WebElement;
-import org.openqa.selenium.support.FindBy;
-import org.openqa.selenium.support.PageFactory;
-import org.openqa.selenium.support.ui.Select;
-
 import com.base.Base;
 
 public class AdminAssignmentPage extends Base {
-	@FindBy (id = "ManageAssigns") WebElement ManageAssigns;
-	@FindBy(id = "AddAssign") WebElement AddAssign;
-	@FindBy(id = "AssignmentBatch") WebElement AssignBatch ;
-	@FindBy(xpath="//button[contains,text()'AssignmentName']") WebElement AssignName ;
-	@FindBy(xpath="//button[contains,text()'AssignmentDesc']") WebElement AssignDesc ;
-	@FindBy(xpath="//button[contains,text()'AssignmentDate']") WebElement AssignDate ;
-
-
 	
 
 
-
-	public AdminAssignmentPage() {
-		PageFactory.initElements(driver,this);
-		
-	}
-	
-	public void ManageAssignments () {
-		ManageAssigns.click(); //can be replaced with loginpage details
-		AddAssign.click();
-			}
-	
-
-	public void AddAssignments (String batchId, String assignName, String assignDesc , String assignDate, String uploadassign) {
-		Select batchNo = new Select(AssignBatch);
-		batchNo.selectByVisibleText(batchId);
-		AssignName.sendKeys(assignName);
-		AssignDesc.sendKeys(assignDesc);
-		AssignDate.sendKeys(assignDate);
-		
-
-			}
 }
